35ae7cc8dd9284b72d1974d5377f991e11b8a503
MSFirsov <mxfrsv@gmail.com>
2025-05-07 18:35:38 +0300

    fix conflicts

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ env = environ.Env()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Take environment variables from .env file
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Allows to keep applications in apps directory
 sys.path.insert(0, os.path.join(BASE_DIR, "apps"))
