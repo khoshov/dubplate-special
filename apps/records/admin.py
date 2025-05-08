b13265fe90c1373d1b0ad55ccdf3cababb6c800c
MSFirsov <mxfrsv@gmail.com>
2025-05-08 16:04:12 +0300

    add RecordAdmin and button AddRecord in admin panel

--- apps/records/admin.py
+++ apps/records/admin.py
@@ -1,1 +1,15 @@
-# Register your models here.
+from django.contrib import admin
+from .models import Record
+
+
+@admin.register(Record)
+class RecordAdmin(admin.ModelAdmin):
+    list_display = ('title', 'display_artists', 'label', 'release_date', 'format')
+    list_filter = ('format', 'genres', 'styles')
+    search_fields = ('title', 'catalog_number', 'barcode')
+    filter_horizontal = ('artists', 'genres', 'styles')
+
+    def display_artists(self, obj):
+        return ", ".join([artist.name for artist in obj.artists.all()])
+
+    display_artists.short_description = 'Artists'
