8de6cf0f2179c090cc9e938e6a2e9039cebfa844
Dmitry M. <di.mesler.2010@yandex.ru>
2025-05-12 20:28:49 +0300

    Feat: Add Models to admin panel

--- apps/records/admin.py
+++ apps/records/admin.py
@@ -1,1 +1,10 @@
+from .models import Record, Genre, Artist, Label, Style, Track
+from django.contrib import admin
+
 # Register your models here.
+admin.site.register(Genre)
+admin.site.register(Artist)
+admin.site.register(Label)
+admin.site.register(Record)
+admin.site.register(Style)
+admin.site.register(Track)
