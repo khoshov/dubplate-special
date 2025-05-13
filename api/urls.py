377c1ce8e832eef7ef28ce1b521495bc3cec53bc
Dmitry <di.mesler.2010@yandex.ru>
2025-05-13 10:38:16 +0300

    Format: API

--- api/urls.py
+++ api/urls.py
@@ -1,21 +1,35 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 from . import views
-
 
 urlpatterns = [
     path("", views.ApiRoot.as_view(), name=views.ApiRoot.name),
     path("artists/", views.ArtistsList.as_view(), name=views.ArtistsList.name),
-    path("artists/<int:pk>/", views.ArtistsDetail.as_view(), name=views.ArtistsDetail.name),
+    path(
+        "artists/<int:pk>/",
+        views.ArtistsDetail.as_view(),
+        name=views.ArtistsDetail.name,
+    ),
     path("labels/", views.LabelsList.as_view(), name=views.LabelsList.name),
-    path("labels/<int:pk>/", views.LabelsDetail.as_view(), name=views.LabelsDetail.name),
+    path(
+        "labels/<int:pk>/", views.LabelsDetail.as_view(), name=views.LabelsDetail.name
+    ),
     path("genres/", views.GenresList.as_view(), name=views.GenresList.name),
-    path("genres/<int:pk>/", views.GenresDetail.as_view(), name=views.GenresDetail.name),
+    path(
+        "genres/<int:pk>/", views.GenresDetail.as_view(), name=views.GenresDetail.name
+    ),
     path("styles/", views.StylesList.as_view(), name=views.StylesList.name),
-    path("styles/<int:pk>/", views.StylesDetail.as_view(), name=views.StylesDetail.name),
+    path(
+        "styles/<int:pk>/", views.StylesDetail.as_view(), name=views.StylesDetail.name
+    ),
     path("records/", views.RecordsList.as_view(), name=views.RecordsList.name),
-    path("records/<int:pk>/", views.RecordsDetail.as_view(), name=views.RecordsDetail.name),
+    path(
+        "records/<int:pk>/",
+        views.RecordsDetail.as_view(),
+        name=views.RecordsDetail.name,
+    ),
     path("tracks/", views.TracksList.as_view(), name=views.TracksList.name),
-    path("tracks/<int:pk>/", views.TracksDetail.as_view(), name=views.TracksDetail.name),
+    path(
+        "tracks/<int:pk>/", views.TracksDetail.as_view(), name=views.TracksDetail.name
+    ),
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
-from rest_framework import permissions, generics
+from records.models import Artist, Genre, Label, Record, Style, Track
+from rest_framework import generics, permissions
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 
@@ -10,7 +11,6 @@ from .serializers import (
     StyleSerializer,
     TrackSerializer,
 )
-from records.models import Artist, Label, Genre, Style, Record, Track
 
 
 class BaseListCreateAPIView(generics.ListCreateAPIView):
@@ -24,84 +24,86 @@ class BaseRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
 class ArtistsList(BaseListCreateAPIView):
     queryset = Artist.objects.all()
     serializer_class = ArtistSerializer
-    name = 'artists-list'
+    name = "artists-list"
 
 
 class ArtistsDetail(BaseRetrieveUpdateDestroyAPIView):
     queryset = Artist.objects.all()
     serializer_class = ArtistSerializer
-    name = 'artist-detail'
+    name = "artist-detail"
 
 
 class LabelsList(BaseListCreateAPIView):
     queryset = Label.objects.all()
     serializer_class = LabelSerializer
-    name = 'labels-list'
+    name = "labels-list"
 
 
 class LabelsDetail(BaseRetrieveUpdateDestroyAPIView):
     queryset = Label.objects.all()
     serializer_class = LabelSerializer
-    name = 'label-detail'
+    name = "label-detail"
 
 
 class GenresList(BaseListCreateAPIView):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
-    name = 'genres-list'
+    name = "genres-list"
 
 
 class GenresDetail(BaseRetrieveUpdateDestroyAPIView):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
-    name = 'genre-detail'
+    name = "genre-detail"
 
 
 class StylesList(BaseListCreateAPIView):
     queryset = Style.objects.all()
     serializer_class = StyleSerializer
-    name = 'styles-list'
+    name = "styles-list"
 
 
 class StylesDetail(BaseRetrieveUpdateDestroyAPIView):
     queryset = Style.objects.all()
     serializer_class = StyleSerializer
-    name = 'style-detail'
+    name = "style-detail"
 
 
 class RecordsList(BaseListCreateAPIView):
     queryset = Record.objects.all()
     serializer_class = RecordSerializer
-    name = 'records-list'
+    name = "records-list"
 
 
 class RecordsDetail(BaseRetrieveUpdateDestroyAPIView):
     queryset = Record.objects.all()
     serializer_class = RecordSerializer
-    name = 'record-detail'
+    name = "record-detail"
 
 
 class TracksList(BaseListCreateAPIView):
     queryset = Track.objects.all()
     serializer_class = TrackSerializer
-    name = 'tracks-list'
+    name = "tracks-list"
 
 
 class TracksDetail(BaseRetrieveUpdateDestroyAPIView):
     queryset = Track.objects.all()
     serializer_class = TrackSerializer
-    name = 'track-detail'
-
+    name = "track-detail"
 
 
 class ApiRoot(generics.GenericAPIView):
-    name = 'api-root'
+    name = "api-root"
+
     def get(self, request, *args, **kwargs):
-        return Response({
-            'artists': reverse('artists-list', request=request),
-            'labels': reverse('labels-list', request=request),
-            'genres': reverse('genres-list', request=request),
-            'styles': reverse('styles-list', request=request),
-            'records': reverse('records-list', request=request),
-            'tracks': reverse('tracks-list', request=request),
-        })
+        return Response(
+            {
+                "artists": reverse("artists-list", request=request),
+                "labels": reverse("labels-list", request=request),
+                "genres": reverse("genres-list", request=request),
+                "styles": reverse("styles-list", request=request),
+                "records": reverse("records-list", request=request),
+                "tracks": reverse("tracks-list", request=request),
+            }
+        )
