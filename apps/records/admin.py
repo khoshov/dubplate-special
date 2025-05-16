52fbbc9c30a58df70518c1774e2909a65ce23445
Dmitry M. <di.mesler.2010@yandex.ru>
2025-05-16 21:58:53 +0300

    Delete: reg models for admin panel

--- apps/records/admin.py
+++ apps/records/admin.py
@@ -1,11 +1,1 @@
-from django.contrib import admin
-
-from .models import Artist, Genre, Label, Record, Style, Track
-
 # Register your models here.
-admin.site.register(Genre)
-admin.site.register(Artist)
-admin.site.register(Label)
-admin.site.register(Record)
-admin.site.register(Style)
-admin.site.register(Track)
