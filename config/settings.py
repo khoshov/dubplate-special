b7cfe82755e4c1634f2706836d69098ac1f58811
Dmitry M. <di.mesler.2010@yandex.ru>
2025-05-23 13:13:10 +0300

    Add: django-silk

--- config/settings.py
+++ config/settings.py
@@ -308,3 +308,6 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
 }
+
+SILKY_AUTHENTICATION = True
+SILKY_AUTHORISATION = True
