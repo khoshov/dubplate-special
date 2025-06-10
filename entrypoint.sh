d069f938a54c1e5e40743a7a01aa48143ed8f99f
Stanislav Khoshov <khoshov@gmail.com>
2025-06-10 21:09:21 +0300

    clean up a bit

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ set -e
 uv run manage.py migrate --no-input
 
 # Collect static
+uv run manage.py collectstatic --no-input
 
 # Compile Django translation messages (.po -> .mo)
 uv run manage.py compilemessages
