809033d7b5779abab9f33bedadff14a60d06bc93
Dmitry M. <di.mesler.2010@yandex.ru>
2025-06-15 21:42:22 +0300

    Refactoring: Change PAGINATION PAGE_SIZE 10 to 18

--- config/settings.py
+++ config/settings.py
@@ -327,7 +327,7 @@ REST_FRAMEWORK = {
         "rest_framework.filters.SearchFilter",
     ),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 10,
+    "PAGE_SIZE": 18,
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
