34f02b37958bfc5f20510a21ccb72f30572a9311
Dmitry M. <di.mesler.2010@yandex.ru>
2025-09-22 23:21:41 +0300

    Refactoring: entrypoint.sh - locales

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ uv run manage.py migrate --no-input
 uv run manage.py collectstatic --no-input
 
 # Compile Django translation messages (.po -> .mo)
-uv run manage.py compilemessages
+uv run manage.py compilemessages --locale ru --locale en --verbosity 0
 
 # Transfer control to docker-compose "command"
 exec "$@"
